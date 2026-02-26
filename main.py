1393b482b0442bf9add380449bb082ad6d146a3f
sandip saw <sandipsaw449@gmail.com>
2026-02-26 23:56:36 +0530

    commet, variable and naming convention

---
+++ main.py
@@ -0,0 +1,34 @@
+print("namaste youtube we are learning python")
+# comment in python'
+
+# hello sandip it is single line comments
+
+""" now me is baar
+multi line comment ke bare me bata raha hu
+dhyan rakho ye multiple line ko comment kar dega
+bas harna ye hai ki 3 baar double quotes lagao or uske 
+under sab likho - ise ham docs string bhi kahte hai """
+
+# variable in python 
+# In python variable is used as a storage to store things in python you can write anything as a variable name .
+# variable banana bahut easy hota hai kuch bhi name likh do or unke baad equal to ka sign de do lekin iske under kisi chij ko store karna hoga.
+
+name = "sandip saw"
+a = 12 
+
+#  there are three type of rule which are not used in variable
+# 1. you cannot use number at variable start
+# 1example = "nahi hai" ❌
+# example1 = "sahi hai" ✅
+
+# 2. you cannot use space in between variable or befor variable.
+#  count = 10
+
+# 3. you should not use special character in front of variable
+# @example = "bgcxkj" ❌
+
+# Naming Convention
+# you can write variable in python using 3 way.
+sheriyansSchool = "harsh Bhaiya" # camel case 
+SheriyansSchool = "Harsh Bhaiya" # pascal case 
+sheriyans_school = "Harsh Bhaiya" # snake Case -isme sab chota hoga bas underscore lagega
