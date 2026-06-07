2be724ced73ad5367ef9bea9eb3c85047f10ed4e
sandip saw <sandipsaw449@gmail.com>
2026-06-08 00:01:58 +0530

    practise question

--- Python_youtube/exam.py
+++ Python_youtube/exam.py
@@ -1,1 +1,106 @@
+"""import math
 
+d1 = {"a":10,"b":20,"c":30}
+d2 = {"d":40,"e":50,"f":60}
+
+# combine both the dictionary
+
+for i in d2:
+    d1[i] = d2[i]
+
+print(d1) # {'a': 10, 'b': 20, 'c': 30, 'd': 40, 'e': 50, 'f': 60}
+
+# sum of the value of dictionary
+
+sum = 0
+for i in d1:
+    sum += d1[i]
+
+print(f"sum={sum}") #210
+
+s1 = {10,15,20,25}
+s2 = {20,15,30,35}
+
+# write the operation of the following function on a given set
+
+union = s1.union(s2)
+print(union)
+intersection = s1.intersection(s2)
+print(intersection)
+difference = s1.difference(s2)
+print(difference)
+
+# write the operation of the following function on a given dictionary
+
+d = {"a":10,"b":20,"c":30}
+d.pop("b")
+d.update({"a":15})
+print(d.items())
+print(d)
+
+# write a program to find the lcm of two number using function
+
+num1 = int(input("Enter first Number"))
+num2 = int(input("Enter second Number")) 
+
+def Lcm(a,b):
+    return (a * b) // math.gcd(a,b)
+
+print("lcm=",Lcm(num1,num2))
+
+# write a program to find the gcd of two number using function
+
+def gcd(a,b):
+    while b!= 0:
+        a, b = b, a%b
+        print(a,b)
+    return a
+
+print("gcd = ",gcd(num1,num2))
+
+# "Write a program in Python to find the sum of digits of any number."
+
+num = int(input("Enter a number : "))
+def sum_of_digit(num):
+    sum = 0
+    while num > 0:
+        digit = num % 10
+        sum += digit
+        num = num // 10
+    return sum
+
+print("sum=",sum_of_digit(num))
+
+# "An email address is provided: hello@python.org. Using type assignment, split the username and domain from the email address."
+
+email = "hello@pthon.org"
+username , domain = email.split('@')
+print("username:",username)
+print("domain:",domain)
+"""
+
+# write a recursive function to find the factorial of number
+
+num = 6
+fact = 1
+while num > 0:
+    fact *= num
+    num -= 1
+print(fact)
+
+
+
+number = int(input("Enter a number:"))
+
+def Fact(n):
+    if n == 0 or n == 1:
+        return 1 
+    else:
+        return n * Fact(n-1)
+
+
+print("factorial:",Fact(number))
+
+# write a function to display the factor of a given numbe
+
+n = 148
